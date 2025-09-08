85012a15fea303e9d345a0073e10535a8b629618
Khoirul Nasid Furqon <nasidfurqon@gmail.com>
2025-09-08 11:29:40 +0700

    ver: 0.1, feat: add login page

--- lib/presentation/pages/auth/loginPage.dart
+++ lib/presentation/pages/auth/loginPage.dart
@@ -1,4 +1,13 @@
+import 'package:go_router/go_router.dart';
 import 'package:flutter/material.dart';
+import 'package:pov2/config/router/app_routes.dart';
+import 'package:pov2/config/theme/app_color.dart';
+import 'package:pov2/config/theme/app_spacing.dart';
+import 'package:pov2/config/theme/app_text.dart';
+import 'package:pov2/core/widget/custom_button.dart';
+import 'package:pov2/core/widget/custom_card.dart';
+import 'package:pov2/core/widget/custom_layout.dart';
+import 'package:pov2/core/widget/custom_textfield.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,6 +19,98 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/patra-tanpa-bg.png',
+          width: 180,
+          color: AppColor.background,
+        ),
+        centerTitle: true,
+      ),
+      body: AppLayout(
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: CustomCard(
+            padding: EdgeInsets.all(AppSpacing.global),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: AppSpacing.lg),
+                const Text('Sistem PoV', style: AppText.headingPrimary),
+                SizedBox(height: AppSpacing.md),
+                const Text(
+                  'Masuk untuk mengakses sistem Proof-of-Visit Pertamina Patraniaga',
+                  textAlign: TextAlign.center,
+                  style: AppText.caption,
+                ),
+                SizedBox(height: AppSpacing.md),
+                CustomButton(
+                  textStyle: AppText.caption,
+                  title: 'Coba mode demo',
+                  backgroundColor: AppColor.border,
+                  foregroundColor: AppColor.textPrimary,
+                  padding: EdgeInsets.symmetric(
+                    vertical: AppSpacing.xs,
+                    horizontal: AppSpacing.sm,
+                  ),
+                  onPressed: () {
+                    context.goNamed(AppRoutes.dashboard.name);
+                  },
+                ),
+                Padding(
+                  padding: EdgeInsets.all(AppSpacing.global),
+                  child: Column(
+                    children: [
+                      CustomTextfield(
+                        keyboardType: TextInputType.emailAddress,
+                        label: "Email",
+                        hint: "anda@pertamina.com",
+                      ),
+                      SizedBox(height: AppSpacing.md),
+                      CustomTextfield(
+                        keyboardType: TextInputType.visiblePassword,
+                        label: "Password",
+                        hint: "***",
+                        obscureText: true,
+                      ),
+                      SizedBox(height: AppSpacing.md),
+                      CustomButtonFull(
+                        textStyle: AppText.heading3Secondary,
+                        title: 'Masuk',
+                        backgroundColor: AppColor.primary,
+                        padding: EdgeInsets.symmetric(
+                          vertical: AppSpacing.xs,
+                          horizontal: AppSpacing.md,
+                        ),
+                        onPressed: () {},
+                      ),
+                      SizedBox(height: AppSpacing.md),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Butuh akun? '),
+                          GestureDetector(
+                            onTap: () {},
+                            child: const Text(
+                              'Daftar',
+                              style: TextStyle(
+                                color: AppColor.primary,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
